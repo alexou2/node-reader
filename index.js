17d3e862cafda39b37ef4a65c28f3b5201b5cb5b
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-01-25 08:45:09 -0500

    same as previous commit
    wasn't properly commited with the other changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use("/manga", express.static('manga'))
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.render("../index", { path_to_image: '/manga/hori/Horimiya Chapter 6.5 Bonus Manga Myamura - Manganelo_files/' });
+    res.render("../index", { path_to_image: displayPages(mangaName, chapName, pageList) });
 });
 
 // Server setup
@@ -18,53 +18,74 @@ app.listen(3000, (req, res) => {
     console.log("Connected on port:3000");
 });
 
+let mangaName = "hori"
+let chapName = "Horimiya Chapter 6.5 Bonus Manga Myamura - Manganelo_files"
+var pageList = getPages();
 
 
+displayPages(mangaName, chapName, pageList)
 
-//reads and prints files in the selected directory
-var pageList = readFiles();
+
+//does all of the formatting in order to display images instead of a blob of text
+function displayPages(mangaName, chapName, pageList) {
+    let link = ""
+    //  pageList = getPages
+    link = `manga/${mangaName}/${chapName}/${pageList[0]}`
+
+    return link
+}
+
 console.log(pageList)
 
 
+function getPages() {
 
-//lists all files in directory
-function readFiles() {
-    var pageList = [];
-    const testFolder = './manga/hori/Horimiya Chapter 6.5 Bonus Manga Myamura - Manganelo_files';
-    const fs = require('fs');
-
-    fs.readdirSync(testFolder).forEach(file => {
-        pageList.push(file);
-
-    });
-    pageList = filterList(pageList)
-    return pageList;
-}
-
-
-//filters the files givent to it and keeps only jpg files
-function filterList(pageList) {
-    var images = pageList.filter(function (p) {
-        return (p.includes('.jpg'));
-    });
-    return (images)
-}
-
-
-linkImages(pageList)
-
-
-//turns links to images into something that is usable by the html code with <img src>
-function linkImages(pageList) {
-
-    var images = []
-    var i = 0;
-    pageList.forEach(function (entry) {
-        images[i] = `<img src = "${pageList[i]}">"`
-        pageList[i] = images[i]
-
-        i = i + 1;
-    });
+    //reads and prints files in the selected directory
+    var pageList = readFiles();
     console.log(pageList)
+
+
+
+    //lists all files in directory
+    function readFiles() {
+        var pageList = [];
+        const testFolder = './manga/hori/Horimiya Chapter 6.5 Bonus Manga Myamura - Manganelo_files';
+        const fs = require('fs');
+
+        fs.readdirSync(testFolder).forEach(file => {
+            pageList.push(file);
+
+        });
+        pageList = filterList(pageList)
+        return pageList;
+    }
+
+
+    //filters the files givent to it and keeps only jpg files
+    function filterList(pageList) {
+        var images = pageList.filter(function (p) {
+            return (p.includes('.jpg'));
+        });
+        return (images)
+    }
+
+
+    // let links = linkImages(pageList)
+
+
+    //turns links to images into something that is usable by the html code with <img src>
+    function linkImages(pageList) {
+
+        var images = []
+        var i = 0;
+        pageList.forEach(function (entry) {
+            // images[i] = `<img src = "${pageList[i]}">`
+            pageList[i] = images[i]
+
+            i = i + 1;
+        });
+        console.log(pageList)
+        return pageList
+    }
     return pageList
 }
