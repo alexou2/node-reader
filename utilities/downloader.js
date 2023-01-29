e87026736234b16a8f5c61b8f1b34ccd807ac607
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-01-29 09:53:48 -0500

    added function to download

--- utilities/downloader.js
+++ utilities/downloader.js
@@ -2,17 +2,30 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 
+const mangaName = "testing";
+
+//runs the download function
+for (let number = 1; number < 15; number ++){
+download(number)
+}
 
 
-
+//downloads the chapters
+function download(number){
 (async () => {
 
 
-  const mangaName = "testing";
+  
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://chapmanganato.com/manga-jt987302/chapter-15');
+    
+    
+    // await page.goto('https://chapmanganato.com/manga-jt987302/chapter-15');
+    await page.goto(`https://chapmanganato.com/manga-jt987302/chapter-${number}`);
+
+
+    
 
     // Get all the image elements on the page
     const images = await page.$$eval('img', imgs => imgs.map(img => img.src));
@@ -20,7 +33,11 @@ const fs = require('fs');
     
 
 
-    const folderName = `../manga/${mangaName}/Chapter_15`
+    // const folderName = `../manga/${mangaName}/Chapter_15`
+     const folderName = `../manga/${mangaName}/Chapter_${number}`
+    
+
+
     if (!fs.existsSync(folderName)) {
       fs.mkdirSync(`../manga/${mangaName}`)
       fs.mkdirSync(folderName);
@@ -36,3 +53,4 @@ const fs = require('fs');
     await browser.close();
     console.log(`${folderName} has finished downloading`)
 })();
+}
