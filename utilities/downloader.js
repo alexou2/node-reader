085d6f2223356b1c4c558175f8c4387f36659a35
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-02-02 16:43:46 -0500

    downloader now gets its feed from the parser

--- utilities/downloader.js
+++ utilities/downloader.js
@@ -1,55 +1,57 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-let sem = require('semaphore')(10)
+// let sem = requimangaNamere('semaphore')(10)
 
 
 
 
 
-//splits the number of chapter in chunks of 9 chapters to fix memory leak issues
+// //splits the number of chapter in chunks of 9 chapters to fix memory leak issues
 
 
-//number of chapters you want to download (I recommend downloading by chunks of 10 chapters to avoid memory leak)
-const chapterNumber = 1000
-//the name of the folder will have this name
-const mangaName = "one piece"
-// the manganato link for the chapters (remove the chapter number from the url)
-const chapterLink = "https://chapmanganato.com/manga-aa951409/chapter-"
-// if you have more than 9 chapters to download, change this variable, otherwise, don't
-const minChap = 1
+// //number of chapters you want to download (I recommend downloading by chunks of 10 chapters to avoid memory leak)
+// const chapterNumber = 1000
+// //the name of the folder will have this name
+// const mangaName = "one piece"
+// // the manganato link for the chapters (remove the chapter number from the url)
+// const chapterLink = "https://chapmanganato.com/manga-aa951409/chapter-"
+// // if you have more than 9 chapters to download, change this variable, otherwise, don't
+// const minChap = 1
 
 
 
 
-//runs the download function for a set number of times
-for (let number = minChap; number <= chapterNumber; number++) {
-  sem.take(()=>{
-  download(number)
-    console.log(`started chapter ${number}`)
-    });
-  }
+// //runs the download function for a set number of times
+// for (let number = minChap; number <= chapterNumber; number++) {
+//   sem.take(()=>{
+//   download(number)
+//     console.log(`started chapter ${number}`)
+//     });
+//   }
+
 
 
 
 
 //downloads the chapters
-// export 
-function download(number) {
-  (async () => {
+// export function download(number) {
 
+
+
+module.exports = {
+  download : function(chapterLink, number, mangaName, sem){
+  (async () => {
+console.log("started downloading", chapterLink)
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
 
-    // await page.goto('https://chapmanganato.com/manga-jt987302/chapter-15');
-
-
     // await page.goto(`https://chapmanganato.com/manga-aa951409/chapter-${number}`, {waitUntil: 'load', timeout: 0});
 
     //downloads the page 
-    await page.goto(`${chapterLink}${number}`, { waitUntil: 'load', timeout: 0 });
-
+    // await page.goto(`${chapterLink}${number}`, { waitUntil: 'load', timeout: 0 });
+await page.goto(chapterLink)
 
 
 
@@ -83,6 +85,8 @@ function download(number) {
     await page.close()
     await browser.close();
     console.log(`${folderName} has finished downloading`)
-    sem.leave()
+    sem.leave(1)
   })();
+}
+
 }
--- utilities/parser.js
+++ utilities/parser.js
@@ -1,13 +1,15 @@
 const cheerio = require('cheerio');
 const fs = require ('fs');
-import { download } from './downloader';
+// import { download } from './downloader';
 
 const { linkSync } = require("fs");
+const sem = require ('semaphore')(10);
 
 
 // gets the content of a local html file
 // const data = fs.readFileSync('test.html', 'utf8')
 const request = require('request');
+const download = require('./downloader.js')
 
 
 let links = []
@@ -38,8 +40,15 @@ $('.info-image img').each((index, element) => {
     images.push($(element).attr('src'));
 });
 
+links = links.reverse()
 console.log("inks: ", links);
 console.log("images: ", images); 
+
+for (let i = 1; i<= links.length; i++){
+    sem.take(()=>{
+download.download(links[i], i, "one piece", sem)
+    })
+}
 })
 
 
