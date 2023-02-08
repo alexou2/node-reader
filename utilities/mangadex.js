b94beb81496a3c3b42a441d84d359f7ea0a9f426
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-02-08 11:16:21 -0500

    updated mangadex downloader

--- utilities/mangadex.js
+++ utilities/mangadex.js
@@ -9,13 +9,33 @@ const axios = require('axios')
 //I use semaphores in ord not to fill up my ram by downloading every chapter at the same time
 const sem = require('semaphore')(5);//change 5 by any number to change the number of chapters that can be downloaded at the same time
 
+const baseUrl = 'https://api.mangadex.org'
+
+
+
 
 
 module.exports = {
-
     //does a search on mangadex.org and returns the 
-    getMangaID: function (searchedManga) {
+    getMangaID: function (title) {
 
+        (async () => {
+            const resp = await axios({
+                method: 'GET',
+                url: `${baseUrl}/manga`,
+                params: {
+                    title: title
+                }
+            });
+            let mangaID = ((resp.data.data.map(manga => manga.id)));
+
+            console.log(resp.data.data.map(manga => manga.id));
+            return mangaID
+        })();
+
+
+
+        console.log(mangaID)
         return mangaID
     },
 
@@ -37,5 +57,6 @@ module.exports = {
 
 
     }
+
 
 }
---
+++ utilities/test2.js
@@ -0,0 +1,3 @@
+const mangadex = require('./mangadex')
+
+mangadex.getMangaID('horimiya')
