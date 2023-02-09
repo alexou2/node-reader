1ddb4a70a0b7ebe185472e7f5b21c40e97e375a7
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-02-09 15:25:12 -0500

    added support for downloading from mangadex
    (kinda)
    the program will crash and only some images will download

--- utilities/mangadex.js
+++ utilities/mangadex.js
@@ -9,7 +9,7 @@ const { searchByName } = require('./parser');
 const fs = require('fs')
 
 //I use semaphores in order not to fill up my ram by downloading every chapter at the same time. it will stop the program when there is no more semaphores
-const sem = require('semaphore')(5);//change 5 by any number to change the number of chapters that can be downloaded at the same time
+const sem = require('semaphore')(2);//change 5 by any number to change the number of chapters that can be downloaded at the same time
 
 const baseUrl = 'https://api.mangadex.org'
 
@@ -18,6 +18,9 @@ const languages = 'en'// modify this if you want to get chapters in another lang
 
 
 module.exports = {
+
+
+
     //does a search on mangadex.org and returns the 
     getMangaID: async function (title) {
 
@@ -49,6 +52,7 @@ module.exports = {
 
 
 
+
     //gets the chapterIDs for the chapter entered
     getChapters: function (mangaID, mangaName) {
         let chapterName = [];
@@ -68,32 +72,35 @@ module.exports = {
 
             console.log(resp.data.data.map(chapter => chapter.id));
 
-            let chapterIDs = resp.data.data.map(chapter => chapter.id)
+            let chapterID = resp.data.data.map(chapter => chapter.id)
             // let chapterName = resp.data.data.map(chapter => chapter.attributes.title)
 
             // console.log(chapterName)
 
-            //gets the chapter's name
-            for (let k = 0; k < chapterIDs.length; k++) {
+            let chapterTitle = resp.data.data.map(chapter => chapter.attributes.title)
+
+            let chapter = resp.data.data.map(chapter => chapter.attributes.chapter)
 
 
-                chapterName = resp.data.data.map(chapter => chapter.attributes.chapter)
+            //creates formatted chapter name
+            for (let k = 0; k < chapterID.length; k++) {
 
-                // mangaName = `chapter ${k} ${mangaName}`;
-
-
+                chapterName[k] = `chapter ${chapter[k]}_ ${chapterTitle[k]}`;
             }
-            console.log(chapterName)
+
+
+            console.log("formattd chapter names", chapterName)
             // for debuging only. prints the mandaIDs as links
-            printLinks('chapter', chapterIDs)
+            printLinks('chapter', chapterID)
 
 
 
             // calls the download function and passes each chapter
-            for (let j = 0; j < chapterIDs.length; j++) {
-                console.log('\x1b[94m%s\x1b[0m', `started chapter #${j}`)
+            for (let j = 0; j < chapterID.length; j++) {
+                
                 sem.take(() => {
-                    this.downloadPages(chapterIDs[j], mangaName, sem, j)
+                    this.getInfos(chapterID[j], mangaName, chapterName[j], sem, j)
+                    console.log('\x1b[94m%s\x1b[0m', `started ${chapterName[j]}`)
                 })
             }
 
@@ -101,17 +108,68 @@ module.exports = {
     },
 
 
+//gets all of the informations from mangadex in order to download chapters
+    getInfos: function (chapterID, mangaName, chapterName, sem, j) {
 
 
-    downloadPages: function (chapterIDs, mangaName, sem, j) {
+        let host, hash, data, dataSaver;
+
+        (async () => {
+            const resp = await axios({
+                method: 'GET',
+                url: `${baseUrl}/at-home/server/${chapterID}`,
+            });
+
+            host = resp.data.baseUrl;
+            chapterHash = resp.data.chapter.hash;
+            data = resp.data.chapter.data;
+            dataSaver = resp.data.chapter.dataSaver;
+
+
+            this.downloadPages(chapterID, mangaName, chapterName, sem, j, host, chapterHash, data)
+        })();
+    },
+
+
+
+    downloadPages: function (chapterID, mangaName, chapterName, sem, j, host, chapterHash, data) {
+
+
+
+
+
+        //creates a folder for the manga
+        const folderPath = `../manga/${mangaName}/${chapterName}`
+        fs.mkdirSync(folderPath, { recursive: true });
 
 
 
 
 
 
+        (async () => {
+            for (const page of data) {
+                const resp = await axios({
+                    method: 'GET',
+                    url: `${host}/data/${chapterHash}/${page}`,
+                    responseType: 'arraybuffer'
+                });
+
+                fs.writeFileSync(`${folderPath}/${page}`, resp.data);
+            };
+
+            console.log(`Downloaded ${data.length} pages.`);
+        })();
+
+
+
+
+
+
+
+        
+        console.log("\x1b[33m%s\x1b[0m", "  finished ", chapterName, "\n")
         sem.leave(1)
-        console.log("\x1b[33m%s\x1b[0m", "  finished chaptrer", j, "\n")
     },
 
 
