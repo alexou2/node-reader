d0c5a6b031015323de20a1d80d2625a7cbf91132
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-02-09 14:14:32 -0500

    added fetcher for manga's name and chapter's name

--- utilities/mangadex.js
+++ utilities/mangadex.js
@@ -1,8 +1,7 @@
 //downloads chapters from mangadex
 
-// thanks to mangaDe for providing an api to download their chapters
-//you can find the documentation for the api here: api.mangadex/docs
-
+// thanks to mangadex for providing an api to download their chapters
+//you can find the documentation for the api here: api.mangadex.org/docs
 
 
 const axios = require('axios');
@@ -14,7 +13,7 @@ const sem = require('semaphore')(5);//change 5 by any number to change the numbe
 
 const baseUrl = 'https://api.mangadex.org'
 
-
+//will only get chapters translated in this language
 const languages = 'en'// modify this if you want to get chapters in another language
 
 
@@ -38,9 +37,12 @@ module.exports = {
 
             var mangaID = resp.data.data.map(manga => manga.id)
 
+            let mangaName = resp.data.data.map(manga => manga.attributes.title.en)
+            console.log(mangaName)
+
 
             // returns only the first manga returned for simplicity purposes.I might change this later, or I might just forget about is :)
-            this.getChapters(mangaID[0])
+            this.getChapters(mangaID[0], mangaName[0])
         })();
 
     },
@@ -48,8 +50,8 @@ module.exports = {
 
 
     //gets the chapterIDs for the chapter entered
-    getChapters: function (mangaID) {
-        let chapterLinks
+    getChapters: function (mangaID, mangaName) {
+        let chapterName = [];
 
         (async () => {
             const resp = await axios({
@@ -62,37 +64,57 @@ module.exports = {
                     "translatedLanguage[]": languages //will only return one specific language 
                 },
 
-
             });
 
             console.log(resp.data.data.map(chapter => chapter.id));
-            let chapterIDs = resp.data.data.map(chapter => chapter.id)
 
+            let chapterIDs = resp.data.data.map(chapter => chapter.id)
+            // let chapterName = resp.data.data.map(chapter => chapter.attributes.title)
+
+            // console.log(chapterName)
+
+            //gets the chapter's name
+            for (let k = 0; k < chapterIDs.length; k++) {
+
+
+                chapterName = resp.data.data.map(chapter => chapter.attributes.chapter)
+
+                // mangaName = `chapter ${k} ${mangaName}`;
+
+
+            }
+            console.log(chapterName)
             // for debuging only. prints the mandaIDs as links
             printLinks('chapter', chapterIDs)
 
 
 
-
+            // calls the download function and passes each chapter
             for (let j = 0; j < chapterIDs.length; j++) {
                 console.log('\x1b[94m%s\x1b[0m', `started chapter #${j}`)
                 sem.take(() => {
-                    this.downloadPages(chapterIDs[j], sem, j)
+                    this.downloadPages(chapterIDs[j], mangaName, sem, j)
                 })
             }
 
         })();
-        return chapterLinks
     },
 
 
 
-    downloadPages: function (chapterIDs, sem, j) {
+
+    downloadPages: function (chapterIDs, mangaName, sem, j) {
+
+
+
+
 
 
         sem.leave(1)
         console.log("\x1b[33m%s\x1b[0m", "  finished chaptrer", j, "\n")
     },
+
+
 
 
     // gets mangas that correspond to the included tags
