c858f190b2e71d445aff3cefafed51fcc50c2c89
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-02-10 22:54:05 -0500

    Update test2.js

--- utilities/test2.js
+++ utilities/test2.js
@@ -2,4 +2,4 @@ const mangadex = require('./mangadex')
 
 //calls the function
 
-mangadex.getMangaID('three days of happiness')// I only call this function because I can't return the value of mangaID
+const sdf = mangadex.getMangaID('three days of happiness')// I only call this function because I can't return the value of mangaID
