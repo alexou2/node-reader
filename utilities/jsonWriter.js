db1485bace108f194404fcfecb6e78e78f7e6d53
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-02-17 11:58:18 -0500

    created basics for JSON writer

---
+++ utilities/jsonWriter.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+
+
+// let outputContent = () => {
+//     let data = fs.readFileSync('./manga.json', 'utf8')
+//     data = JSON.parse(data)
+
+
+
+//     console.log('name: ', data.mangaName)
+//     console.log(`path: ${__dirname}/${data.path}`)
+//     console.log('chapter number:', data.chapter_count)
+//     console.log(data.chapters.map(chap => chap.chapter1.map(path => path.path)))
+// }
+// outputContent()
+
+
+modules.exports = {
+
+
+    getName: function (path) {
+        let data = fs.readFileSync(path, 'utf8')
+        data = JSON.parse(data)
+        return data.mangaName
+    },
+
+    getPath: function (path) {
+        let data = fs.readFileSync(path, 'utf8')
+        data = JSON.parse(data)
+        return data.path
+    },
+
+    getChapters: function (path) {
+        let data = fs.readFileSync(path, 'utf8')
+        data = JSON.parse(data)
+        return data.chapters.map(chap => chap)
+    },
+
+
+
+    // writes a new json file for each manga
+    addManga: function (mangaName, path, chapter_count, chapterNameList, chapterPathList, tags, description) {
+        let chapters = []
+
+        // gives attributes to each chapter
+        for (let i = 0; i < chapterNameList.length; i++) {
+            chapters.push({ sorting_order: i + 1, chapterName: chapterNameList[i], chapterPath: path + "/" + chapterPathList[i] })
+        }
+
+
+        // the attributes of the file
+        let mangaJSON = {
+            mangaName: mangaName,
+            path: path,
+            cover_path: path + "/cover.jpg",
+            chapter_count: chapters.length,
+            tags: tags,
+            description: description,
+            chapters: chapters,
+        }
+
+        // convert JSON object to a string
+        const manga = JSON.stringify(mangaJSON, 'null', 2)
+
+
+
+        //writes the files
+        fs.writeFileSync(`jsonFiles/${mangaName}.json`, (manga), err => {
+            if (err) {
+                console.log(`Error writing file: ${err}`)
+            } else {
+                console.log(`File is written successfully!`)
+            }
+        })
+
+    },
+
+
+    // updates the manga with new infos
+    updateManga: function (newInfos) {
+
+
+    },
+
+
+}
