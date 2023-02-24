92d5567bb2d6b7b381c5dfdcba10f5240f94c406
alexou2 <92472779+alexou2@users.noreply.github.com>
2023-02-24 15:31:47 -0500

    added elements to todo lists for ux improvements in html pages

--- utilities/jsonWriter.js
+++ utilities/jsonWriter.js
@@ -7,10 +7,11 @@ const sanitizeFilename = require('sanitize-filename');
 // finish function for checking if all mangas have json files
 // add json files for manganato
 // have only one instance for each manga in manga list
-    // if offset for new chapters of existing manga >= existing chapters in json: add chapters at end of list
-    // if offset is lower: write complete json
-    // if last new downloaded chapter < old first chapter: add new feed and then add old data
-    // change sort order from i to the chapter number from mangadex
+        // if offset for new chapters of existing manga >= existing chapters in json: add chapters at end of list
+        // if offset is lower: write complete json
+        // if last new downloaded chapter < old first chapter: add new feed and then add old data
+// change sort order from i to the chapter number from mangadex
+// 
 
 
 
@@ -114,7 +115,7 @@ module.exports = {
                 chapterPath: path + "/" + chapterPathList[i],
             })
         }
-        console.log('chapter paths:',chapterPathList)
+        console.log('chapter paths:', chapterPathList)
 
         // the attributes of the file
         let mangaJSON = {
@@ -206,7 +207,7 @@ module.exports = {
 
 
         chapterNameList = fs.readdirSync(`manga/${mangaName}`)
-        
+
         chapterPathList = fs.readdirSync(`manga/${mangaName}`)
         for (let i = 0; i < chapterPathList.length; i++) {
             chapterPathList[i] = `manga/${chapterPathList[i]}`
