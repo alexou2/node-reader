f97012a215509f86ba435e52cf9f76c338d85781
alexou2 <92472779+alexou2@users.noreply.github.com>
2023-03-08 10:41:36 -0500

    added javascript file to manage interactions on client-side

---
+++ ressources/pageInteractions.js
@@ -0,0 +1,27 @@
+// page interactions that run on the client side
+
+
+// adds a bookmark to the chapter
+function addBookmark(mangaName, chapter) {
+
+}
+
+// removes the bookmark
+function removeBookmark(mangaName, chapter) {
+
+}
+
+// finds which chapter is bokmarked and returns it as a list
+function getBookmark(mangaName) {
+
+}
+
+// saves the progress in the manga to resube later
+function followProgress(mangaName, chapter) {
+
+}
+// when the button is clicked, it will download the newest chapters for the manga
+function updateMangaChaps(mangaName) {
+
+}
+
