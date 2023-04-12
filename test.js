86405bfe41a5bf1e22f0ff572b2cf4ec69508839
alexou2 <92472779+alexou2@users.noreply.github.com>
2023-04-12 10:05:38 -0400

    changed test and z-index

--- test.js
+++ test.js
@@ -1,43 +1,10 @@
-const jsonWriter = require('./utilities/jsonWriter');
+const manganato = require('./utilities/parser');
 
-// const paragraph = 'ch-1_p1 ch12 ch 2';
-const data = jsonWriter.getMangaJson('Kishuku Gakkou no Juliet');
+console.log('---------------------------------------------------');
+// (async () => {
+    const searchLink = manganato.searchByName('three days of happiness')
+    console.log(searchLink)
 
-let paragraph = data.chapters.map(path => path.chapterPath);
-
-console.log('type:', typeof (data))
-
-console.log(data)
-
-// let tags = data.tags;
-// let mangaDesc = data.description;
-//  paragraph = data.chapters.map(name => name.chapterPath);
-// let bookmarks = data.chapters.map(book => book.bookmarked);
-
-// let t = "Chapter .*"
-
-// // const regex = /(Ch.?.?.?.?.?.?.?\d+(\.\d)*.*)/ig;
-//  let regex = new RegExp(`${t}.?.?.?.?.?.?.?\d+(\.\d)*.*`,"ig")
-//  regex = new RegExp(`${t}`, 'ig')
-
-// // const regex = /Chapter\s*(\d+(?:\.\d+)?)/i;
-
-// console.log('para:',paragraph);
-// let found = []
-// for (i in paragraph){
-// found.push(paragraph[i].match(regex))
-// console.log('matches:',paragraph[i].match(regex))
-
-// }
-// // console.log(found);
-
-
-// // found = JSON.stringify(found)
-// console.log(typeof(found))
-// console.log(found.length)
-
-
-// //let matches = found.split(' ')
-// // for (let i in found)
-// // consolee.log(found[0]+'\n')
-// // Expected output: Array ["T", "I"]
+    const mangaLink = (manganato.getMangaLink(searchLink))
+    console.log(mangaLink)
+// })
